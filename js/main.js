0ef10c4b2b4c7c6b9447109e5c1f4f2f60fb8b9f
Bîrsanu Olga <88458123+olgabirsanu@users.noreply.github.com>
2022-06-07 21:56:59 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,42 @@
+new SimpleLightbox({ elements: '.container-galerie a' });
+new SimpleLightbox({ elements: '.orar-imagini a' });
+
+AOS.init({
+    duration: 700,
+    offset: 100,
+    once: true
+});
+
+
+emailjs.init("WudV94eFG9b7HuDz5");
+
+function mobile() {
+    var x = document.getElementById("navbar");
+    if (x.className === "nav") {
+        x.className = "nav mobile";
+    } else {
+        x.className = "nav";
+    }
+}
+
+function send() {
+
+    if (document.getElementById("formular").checkValidity() == false) {
+        document.getElementById("formular").reportValidity();
+        return;
+    }
+
+    var date = {
+        nume: document.getElementById("nume").value,
+        email: document.getElementById("email").value,
+        mesaj: document.getElementById("mesaj").value
+    };
+
+    emailjs.send("service_0zbtr34", "template_duympib", date)
+        .then(function (raspuns) {
+            alert("Mesajul a fost transmis.")
+            document.getElementById("formular").reset();
+        }, function (error) {
+            alert("Eroare la transmitere. Contactati programatorul.")
+        })
+}
